c726a6630dd2d4f7211cf5b316f34379064d9711
Daniel Hoherd <daniel.hoherd@gmail.com>
2016-05-03 15:57:51 -0700

    Purging old puppet before beginning new puppet installation

--- docker-puppetmaster/setup-puppet-server.sh
+++ docker-puppetmaster/setup-puppet-server.sh
@@ -3,6 +3,7 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
+apt-get purge -y puppet-common
 apt-get install -y wget rsync
 wget --no-verbose https://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb
 dpkg -i puppetlabs-release-pc1-trusty.deb
