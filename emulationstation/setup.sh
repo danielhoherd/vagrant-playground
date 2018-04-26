e44fd438f19d796a41de21fb68cda63940579a9b
Daniel Hoherd <daniel.hoherd@renovo.auto>
2018-04-26 12:44:33 -0700

    Add Makefile. Run pre-commit on all files.

--- emulationstation/setup.sh
+++ emulationstation/setup.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 
 sudo apt-get autoremove -y
 
-# Install emulationstation, then its dependencies. Would be cool to have a way 
+# Install emulationstation, then its dependencies. Would be cool to have a way
 # to do this in one step, eg: yum localinstall -y packagename
 wget --no-verbose http://emulationstation.org/downloads/releases/emulationstation_amd64_latest.deb
 sudo dpkg --install emulationstation_amd64_latest.deb
@@ -17,4 +17,3 @@ sudo apt-get install -y \
   silversearcher-ag \
   vim \
   wget
-
